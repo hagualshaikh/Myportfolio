2af28e8b25e524ef12d9f4953bfb0ef701d8257c
hagualshaikh <hagualshekh@gmail.com>
2020-09-07 20:49:30 +0200

    ini package

--- src/Components/Aboud/index.js
+++ src/Components/Aboud/index.js
@@ -18,7 +18,9 @@ const Aboud = () => {
                     <InfoTitle ><Span>This is</Span> Me</InfoTitle>
                     <InfoDir >Creative Director</InfoDir>
                     <InfoDesc >
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est <InfodescA href="#">explicabo</InfodescA> maxime quibusdam quaerat nihil aliquid aperiam deleniti, repellendus dolorum. Possimus nisi cupiditate ad veritatis?
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est $ git remote -v
+> origin  git@github.com:USERNAME/REPOSITORY.git (fetch)
+> origin  git@github.com:USERNAME/REPOSITORY.git (push)<InfodescA href="#">explicabo</InfodescA> maxime quibusdam quaerat nihil aliquid aperiam deleniti, repellendus dolorum. Possimus nisi cupiditate ad veritatis?
                     </InfoDesc>
                     <InfoDesc >
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim est explicabo maxime quibusdam quaerat nihil aliquid aperiam deleniti, repellendus dolorum. Possimus nisi cupiditate ad veritatis?
